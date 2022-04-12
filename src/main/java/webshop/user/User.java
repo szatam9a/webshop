a84df1d0fe7917459b961ed593a8debe50f53b64
Szalay Tamás <szalaytamas90@gmail.com>
2022-04-12 17:47:08 +0200

    init

--- src/main/java/webshop/user/User.java
+++ src/main/java/webshop/user/User.java
@@ -3,4 +3,51 @@ package webshop.user;
 public class User {
     private int ID;
     private String name;
+    private String emailAddress;
+    private int password;
+
+    public User(int ID, String name, String emailAddress, int password) {
+        this.ID = ID;
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public User(String name, String emailAddress, int password) {
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
 }
